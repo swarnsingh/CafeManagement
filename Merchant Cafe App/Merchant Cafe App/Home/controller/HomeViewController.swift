3f5ffb033cd0e2bdd0662f72ca3feba1692fc779
Umang Gupta <umang.gupta@intimetec.com>
2018-06-05 15:33:17 +0530

    Added Side MEnu Code

--- Merchant Cafe App/Merchant Cafe App/Home/controller/HomeViewController.swift
+++ Merchant Cafe App/Merchant Cafe App/Home/controller/HomeViewController.swift
@@ -3,43 +3,44 @@
  */
 
 import UIKit
+import SideMenuSwift
 
 class HomeViewController: UIViewController {
     
     
+    @IBOutlet var categoryBtn: UIButton!
     @IBOutlet weak var logOutBtn: UIButton!
     
     @IBOutlet weak var productsBtn: UIButton!
     
-    @IBOutlet var Categorybtn: UIButton!
+    
     override func viewWillAppear(_ animated: Bool) {
         if !PreferenceManager.isUserLogin() {
             self.goToController(controller: Constants.LOGIN_SEGUE, nextViewController: LoginViewController.self)
         }
     }
-
+    
     
     override func viewDidAppear(_ animated: Bool) {
-        if Connectivity.isConnectedToInternet {
-            Constants.db.collection("Admin").document("1").addSnapshotListener(
-            includeMetadataChanges: true) { querySnapshot, error in
-                guard let documents = querySnapshot?.get("account_info") else {
-                    print("Error fetching account_info documents: \(error!)")
-                    return
-                }
-                let account_info: [String:Any] = documents as! [String : Any]
-                
-                let deviceId = account_info["device_id"] as! String
-                
-                if deviceId != UIDevice.current.identifierForVendor?.uuidString {
-                    if (PreferenceManager.isUserLogin()) {
-                        PreferenceManager.setUserLogin(isUserLogin: false)
-                        self.goToController(controller: Constants.LOGIN_SEGUE, nextViewController: LoginViewController.self)
-                        self.showAlert("User logged in from another device. Please login again.")
-                    }
-                }
-                print("Account Info : \(String(describing: account_info["device_id"]))")
+        Constants.db.collection("Admin").document("1").addSnapshotListener(
+        includeMetadataChanges: true) { querySnapshot, error in
+            guard let documents = querySnapshot?.get("account_info") else {
+                print("Error fetching account_info documents: \(error!)")
+                return
             }
+            let account_info: [String:Any] = documents as! [String : Any]
+            
+            let deviceId = account_info["device_id"] as! String
+            
+            if deviceId != UIDevice.current.identifierForVendor?.uuidString {
+                if (PreferenceManager.isUserLogin()) {
+                    self.showAlert("User logged in from another device. Please login again.")
+                    PreferenceManager.setUserLogin(isUserLogin: false)
+                    self.goToController(controller: Constants.LOGIN_SEGUE, nextViewController: LoginViewController.self)
+                    
+                }
+            }
+            print("Account Info : \(String(describing: account_info["device_id"]))")
         }
     }
     
@@ -52,6 +53,12 @@ class HomeViewController: UIViewController {
     
     @IBAction func onProductClick(_ sender: Any) {
         self.performSegue(withIdentifier: Constants.CATEGORY_PRODUCT_SEGUE, sender: nil)
+    }
+    
+    @IBAction func sidmenuButtonPressed(_ sender:UIButton){
+        
+        self.sideMenuController?.revealMenu()
+        
     }
     
     @IBAction func onCategoryPress(_ sender: UIButton){
@@ -67,28 +74,28 @@ class HomeViewController: UIViewController {
         PreferenceManager.setUserLogin(isUserLogin: false)
         self.goToController(controller: Constants.LOGIN_SEGUE, nextViewController: LoginViewController.self)
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.Categorybtn.layer.cornerRadius = 10
         self.navigationController?.navigationBar.tintColor = .white
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
